074db73be44568fbc227be9f44774956bc770790
Miles <yw@deepnatural.ai>
2024-05-02 23:35:02 +0900

    huggingface_hub 의 list_models() 를 사용하여, 실시간 Model 목록 확인
    * 검색 task: `text-classification`

---
+++ 03-start-langchain/03-07 list_models (text-classification).py
@@ -0,0 +1,13 @@
+from huggingface_hub import list_models
+
+def list_most_popular(task: str):
+    for rank, model in enumerate(list_models(
+        filter=task,
+        sort="downloads",
+        direction=-1,
+    )):
+        if rank == 5:
+            break
+        print(f'{model.id}, {model.downloads}')
+
+list_most_popular('text-classification')
